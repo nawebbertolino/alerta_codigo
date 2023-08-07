4b5be20d022bf314bf083e6e80057c25bfb85f58
Tânia <139794741+nawebbertolino@users.noreply.github.com>
2023-08-07 16:23:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var idade, eleitor, resultado
+idade = 71;
+idade = 65;
+eleitor = (idade <18)
+
+//function Soma( a, b ) {
+ 
+ //return  a + b ;
+//}
+
+//alert(Soma (3,6))
+//console.log(Soma(3,6))
+
+// no resultado devo colocar o sinal que representa OU entre os grupos de parenteses
+//no resultado devo colocar o sinal que representa && entre os grupos de parenteses
+//resultado = (idade === 65) &&( idade === 71); // true or false
+//alert (resultado);
+
+function Soma(a,b) {
+    if ( a + b === 50) {
+     console.log( (a+ b) + "e igual a 50");
+    } else {
+    console.log( (a+ b ) + " nao e igual a 50");
+}
+}
+console.log(Soma (10,10))
+
